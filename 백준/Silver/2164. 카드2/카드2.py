5891e95ecfe410f6590b262fa93fd3290fa970cc
seokjea <tjrwo218@naver.com>
2025-02-28 21:56:44 +0900

    [Silver IV] Title: 카드2, Time: 168 ms, Memory: 51888 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,9 @@
+from collections import deque
+n = int(input())
+                                      
+que = deque(range(1,n+1))
+while len(que) >1:
+    que.popleft()
+    que.append(que.popleft())
+print(que.pop())
+         
